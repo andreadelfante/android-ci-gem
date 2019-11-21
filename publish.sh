0196aa460a80bcc15d024034f8ce142315938ce3
Andrea Del Fante <andreadelfante94@gmail.com>
2019-11-21 20:32:43 +0100

    Version 29.0.2 ready

--- publish.sh
+++ publish.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 BASE=andreadelfante/android-ci-gem
+VERSION_BASE=29.0.2
+LATEST_BASE=latest
+
 RC=$BASE:rc
-LATEST=$BASE:latest
-VERSION=$BASE:29.0.2
+LATEST=$BASE:$LATEST_BASE
+VERSION=$BASE:$VERSION_BASE
 
 docker rmi $RC --force
 docker rmi $LATEST --force
@@ -11,10 +14,23 @@ docker rmi $VERSION --force
 
 docker build -t $RC .
 
-docker tag $RC $LATEST
 docker tag $RC $VERSION
+docker tag $RC $LATEST
 
 docker login
-docker push $LATEST
 docker push $VERSION
+docker push $LATEST
 
+git add .
+git commit -m "Version ${VERSION_BASE} ready"
+git push
+
+git tag -d $LATEST_BASE
+git push --delete origin $LATEST_BASE
+
+git tag -d $VERSION_BASE
+git push --delete origin $VERSION_BASE
+
+git tag $LATEST_BASE
+git tag $VERSION_BASE
+git push origin --tags
