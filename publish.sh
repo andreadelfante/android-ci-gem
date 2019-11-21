f62d779a36822a2a7db9b34cfe80d14852bbed66
Andrea Del Fante <andreadelfante94@gmail.com>
2019-11-21 19:21:59 +0100

    Version 28.0.3 ready

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 BASE=andreadelfante/android-ci-gem
+VERSION_BASE=28.0.3
+
 RC=$BASE:rc
-VERSION=$BASE:28.0.3
+VERSION=$BASE:$VERSION_BASE
 
 docker rmi $RC --force
 docker rmi $VERSION --force
@@ -14,3 +16,12 @@ docker tag $RC $VERSION
 docker login
 docker push $VERSION
 
+git add .
+git commit -m "Version ${VERSION_BASE} ready"
+git push
+
+git tag -d $VERSION_BASE
+git push --delete origin $VERSION_BASE
+
+git tag $VERSION_BASE
+git push origin --tags
