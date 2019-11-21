bd06d0683ebc2a533bca40c80dcb939d8c725053
Andrea Del Fante <delfante@spaggiari.eu>
2019-11-21 18:08:33 +0100

    Update new image for 28.0.3

--- publish.sh
+++ publish.sh
@@ -2,19 +2,15 @@
 
 BASE=andreadelfante/android-ci-gem
 RC=$BASE:rc
-LATEST=$BASE:latest
-VERSION=$BASE:29.0.2
+VERSION=$BASE:28.0.3
 
 docker rmi $RC --force
-docker rmi $LATEST --force
 docker rmi $VERSION --force
 
 docker build -t $RC .
 
-docker tag $RC $LATEST
 docker tag $RC $VERSION
 
 docker login
-docker push $LATEST
 docker push $VERSION
 
